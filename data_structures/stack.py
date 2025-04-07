aebf263064c66ad0f2401924c4e489bd394aedaf
Danielle Kapsa <danielle.b.selambi@aims-senegal.org>
2025-04-07 20:33:39 +0000

    commit all changes

--- data_structures/stack.py
+++ data_structures/stack.py
@@ -1,1 +1,22 @@
-#test
+class Stack:
+    def __init__(self, para=[]):
+      self.stack = para
+      pass
+
+    def push(self,newElement):
+      self.stack.append(newElement)
+    #  return self.para.append(self,newElement)
+
+    def pop(self):
+      last.element = self.stack[len(self.stack)-1]
+      self.stack.remove(len(self.stack)-1)
+      return last.element
+
+    def top(self):
+      self.stack[len(self.stack)-1]
+
+    def isEmpty(self):
+      len(self.stack)==0
+
+    def display_stack(self):
+      print(self.Stack)
